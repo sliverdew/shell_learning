9f262f64a329b2d3685ce2ab51abf0e6aa5b0445
碗里 <shine.sliverdew@gmail.com>
2016-11-03 18:15:01 +0800

    Add files via upload

---
+++ #monkey.sh
@@ -0,0 +1,16 @@
+#自动monkey
+touch anr.txt
+chmod 777 anr.txt
+touch crashed.txt
+chmod 777 crashed.txt
+start=$(date "+%s")
+logcat -b system -b event -b main -b radio -b crash -v time -f a.alog&
+cat /proc/kmsg > k.klog &
+monkey --throttle 200 --ignore-crashes --ignore-timeouts --ignore-security-exceptions -v -v 1000 >m.log &
+#数据分析
+now=$(date "+%s")
+time=$((now-start))
+echo "time used:$time seconds"
+cat a.log | grep "Application is not responding" | awk -F  '{ print $13 "," $0}' $* |sort > anr.txt #按异常package包名排序
+cat a.log | grep "am_crash" | awk -F  '{ print $8 "," $0}' $* |sort > crashed.txt #按异常package包名排序
+cat m.log | grep "sending event" | tail -n 1
