77d93b7e5b5a2031fb513a3869003512bd59fabc
碗里 <shine.sliverdew@gmail.com>
2016-11-09 08:44:06 +0800

    Add files via upload

--- Analysis.sh
+++ Analysis.sh
@@ -1,4 +1,7 @@
 #数据分析
-cat alog.txt | grep "Application is not responding" | awk '{ print $13 " " $0}' $* |sort|awk -f ANRan.sh >> result.txt #按异常package包名排序
-cat alog.txt | grep "am_crash" | awk '{ print $8 "," $0}' $* |sort| awk -f ANRan.sh >> result.txt #按异常package包名排序
-echo m.log | grep "sending event" | tail -n 1 >> result.txt #直接取最后一次结果判断运行次数
+now=$(date "+%s")
+time=$((now-start))
+echo "time used:$time seconds"
+cat a.log | grep "Application is not responding" | awk '{ print $13 "," $0}' $* |sort > anr.txt #按异常package包名排序
+cat a.log | grep "am_crash" | awk '{ print $8 "," $0}' $* |sort > crashed.txt #按异常package包名排序
+echo m.log | grep "sending event" | tail -n 1 #直接取最后一次结果判断运行次数
