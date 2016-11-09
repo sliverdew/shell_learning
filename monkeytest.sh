4cf6597e108338f84d9308a4f37bd07fd283da2d
碗里 <shine.sliverdew@gmail.com>
2016-11-09 08:43:25 +0800

    Add files via upload

--- monkeytest.sh
+++ monkeytest.sh
@@ -1,4 +1,4 @@
 ﻿#自动monkey
 logcat -v time -f alog.txt&
 cat /proc/kmsg > klog.txt &
-monkey --throttle 200 --ignore-crashes --ignore-timeouts --ignore-security-exceptions -v -v 100000000 >m.log &
+monkey --throttle 200 --ignore-crashes --ignore-timeouts --ignore-security-exceptions -v -v 5000000000 >m.log &
